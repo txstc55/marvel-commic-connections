33d0661e93a6f3880764973d4c6e474a1c2c2f83
Xuan Tang <txstc55@gmail.com>
2021-08-16 21:22:50 +0800

    I am lazy af

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>I am too lazy to write an about page</h1>
+    <h2>
+      Here, give a star at the
+      <a
+        href="https://github.com/txstc55/marvel-commic-connections"
+        target="_blank"
+        >github page</a>
+      if you want<br>Thank you
+    </h2>
   </div>
 </template>
