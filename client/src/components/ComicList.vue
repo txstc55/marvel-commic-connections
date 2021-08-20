5d58b083c32b90fab47b2d808900ecdc64fe539b
Xuan Tang <txstc55@gmail.com>
2021-08-20 10:53:12 +0800

    change position for date

--- client/src/components/ComicList.vue
+++ client/src/components/ComicList.vue
@@ -62,7 +62,8 @@
                       :key="authorID"
                       v-html="authorURL(authorID)"
                     ></span>
-                    <br>
+                  </v-card-subtitle>
+                  <v-card-subtitle>
                     Published On: {{ comics[comicID].date }}
                   </v-card-subtitle>
                   <v-card-actions>
