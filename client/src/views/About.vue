26007397a2cd41b81fd590347876c7673f5fec1b
Xuan Tang <txstc55@gmail.com>
2021-08-28 11:36:26 +0800

    add more about

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="about">
     <h1>I am too lazy to write an about page</h1>
+    <br />
     <h2>
       Here, give a star at the
       <a
         href="https://github.com/txstc55/marvel-commic-connections"
         target="_blank"
-        >github page</a>
-      if you want<br>Thank you
+        >github project page</a
+      >
+      if you want
+    </h2>
+    <br />
+    <h2>
+      Visit my
+      <a href="https://txstc55.github.io" target="_blank">github page</a> for
+      more fun projects
     </h2>
   </div>
 </template>
